c3a50ddf0267565103dbfef3b7499603990465f4
willfraisl <willfraisl@gmail.com>
2018-11-18 12:17:49 -0800

    proj8

--- proj8.c
+++ proj8.c
@@ -1,45 +1,12 @@
 /*
-Basics of program needed to complete assignment. 
-This example processes the first logical address stored in addresses.txt.
-It translates the address to a physical address and looks up that physical address in BACKING_STORE.bin
-The output is identical to the first line of correct.txt
-usage: ./a.out BACKING_STORE.bin addresses.txt
-BACKING_STORE.bin and addresses.txt must bin the same directory as the executable
-
-Your task is to complete the program for all virtual adresses in addresses.txt.  Think of a loop reading addresses.txt 
-as a memory bus passsing logical addresses to a program.   
-
-See Figure 8.34 on p. 433.   The page number/offset block is what comes from addresses.txt.   
-Here are the conditions that must be accounted for:
-
-1. logical addreess is in TLB
-2. logical address in not in TLB but is in memory
-3. logical address is neither in TLB nor in memory
-
-
-
-while(there are more logical addresses)
-{
- extract the page number and offset from the logical address. 
- Search the TLB for the page number.
- If the page number is in the TLB, add the offset to the frame number found in the TLB. This is the physical address. 
-   Use it to extract data from memory.
-   Print out according to instructions in the text. 
- If the page number is not in the TLB, use the page number as an offset into the page table.
- If the page number is in the page table, put the page number and the frame number in the TLB. Add the offset to the
-   page frame number found in the page table.  This is the physical address.  Use it to extract data from memory. 
- If the page number is not in the page table, bring the page in from BACKING_STORE.bin. You do this by translating the 
-   address, just as I do in the example below. Add it to memory.  Update the page table and TLB.   
-   Print out according to instructions in the text. 
-}
-
-This program simulates demand paging.  This means that memory is initially empty.  The program will page fault until
-memory is full.  Since memory is the same size as BACKING_STORE, you do not have to handle page replacement. TLB is 
-another story.  If you access a page that is not in TLB and TLB is full, you must evict an entry in the TLB.  
-This means you need an eviction strategy.  Use LRU.  Include an integer in the TLB that will store the most recent 
-address reference.  For example, there are 1000 addresses stored in addresses.txt.  If you get a TLB  hit 
-(or store an entry in the TBL) on 77th counting from 0, store 77 in the TLB struct for that page.  If you need to evict
-a page from the TLB, overwrite the TLB entry with the lowest associated number. 
+Class: CPSC 346-02
+Team Member 1: Will Fraisl
+Team Member 2: N/A 
+GU Username of project lead: wfraisl
+Pgm Name: proj8.c
+Pgm Desc: simulates virtual memory using page table
+Usage: ./a.out BACKING_STORE.bin addresses.txt
+Due Date: 11/20/18
 */
 
 #include <stdio.h>
@@ -57,7 +24,6 @@ a page from the TLB, overwrite the TLB entry with the lowest associated number.
 #define OFFSET_BITS 8
 #define OFFSET_MASK 255
 #define MEMORY_SIZE PAGES * PAGE_SIZE 
-
 
 /*
 ********Important Types, Data Structures and Variables************************ 
@@ -91,20 +57,9 @@ Physical address: physical_address in Addresses, above
 value: value stored in main_memory at physical_page displacement plus offset
 */
 
-struct tlbentry 
-{
-    unsigned char page_number;
-    unsigned char frame_number;
-    int tlb_ref;
-};
-typedef struct tlbentry tlbentry;  
-
-
-
 int main(int argc, char *argv[])
 { 
     int pagetable[PAGES];
-    tlbentry tlb[TLB_SIZE];
     signed char main_memory[MEMORY_SIZE];
     signed char *backing;
     int logical_address;
@@ -116,6 +71,9 @@ int main(int argc, char *argv[])
     int numPageRefs = 0;
     int numTLBRefs = 0;
     int numTLBHits = 0;
+    int i;
+    int found;
+    int address_count = 0;
 
     //open simulation of secondary storage     
     const char *backing_filename = argv[1]; 					//BACKING_STORE.bin 
@@ -125,36 +83,47 @@ int main(int argc, char *argv[])
     //open simulation of address bus and read the first line 
     FILE *ifp = fopen(argv[2],"r"); 						//addresses.txt 
 
-    while(fscanf(ifp,"%d", &logical_address) != EOF){
+    for(i=0; i<256; i++){
+        pagetable[i] = 0;
+    }
 
-        /*
-        If the page number is not in the TLB, use the page number as an offset into the page table.
-        If the page number is in the page table, put the page number and the frame number in the TLB. Add the offset to the
-        page frame number found in the page table.  This is the physical address.  Use it to extract data from memory. 
-        If the page number is not in the page table, bring the page in from BACKING_STORE.bin. You do this by translating the 
-        address, just as I do in the example below. Add it to memory.  Update the page table and TLB.   
-        Print out according to instructions in the text. 
-        */
+    physical_page = 0; 
+
+    while(fscanf(ifp,"%d", &logical_address) != EOF){
+        numPageRefs++;
 
         //extract low order 8 bits from the logical_address. 
         offset = logical_address & OFFSET_MASK;
 
         //extract bits 8 through 15. This is the page number gotten by shifting right 8 bits 
         logical_page = (logical_address >> OFFSET_BITS) & PAGE_MASK;
+        
+        found = 0;
 
-        //When you write the program, the physical page will increment by 1 for each copy
-        //from simulated backing store to main memory 
-        physical_page = 0; 
+        //search through pagetable for logical_address
+        for(i=0; i<=physical_page; i++){
+            if(pagetable[i] == logical_page){
+                found = 1;
+                physical_address = (i << OFFSET_BITS) | offset;
+                break;
+            }
+        }
 
-        //copy from secondary storage to simulated RAM. The address on secondary storage
-        //is an offset into backing, computed by multiplying the logical
-        //page number by 256 and adding the offset 
-        memcpy(main_memory + physical_page * PAGE_SIZE, 
-                backing + logical_page * PAGE_SIZE, PAGE_SIZE);
+        if(found == 0){
+            numPageFaults++;
+            //copy from secondary storage to simulated RAM. The address on secondary storage
+            //is an offset into backing, computed by multiplying the logical
+            //page number by 256 and adding the offset 
+            physical_page++;
+            memcpy(main_memory + physical_page * PAGE_SIZE, 
+                    backing + logical_page * PAGE_SIZE, PAGE_SIZE);
 
-        //Shift the physical page left 8 bits and or with the offset
-        //This has the effect of adding the offset to the physical_page
-        physical_address = (physical_page << OFFSET_BITS) | offset;
+            //Shift the physical page left 8 bits and or with the offset
+            //This has the effect of adding the offset to the physical_page
+            physical_address = (physical_page << OFFSET_BITS) | offset;
+            
+            pagetable[physical_page] = logical_page;
+        }
 
         //extract the value stored at offset bytes within the page
         signed char value = main_memory[physical_page * PAGE_SIZE + offset];
@@ -162,5 +131,8 @@ int main(int argc, char *argv[])
         printf("Virtual address: %d Physical address: %d Value: %d\n", 
                 logical_address, physical_address, value);
     }
+    printf("Number of Translated Addresses = %d\n", numPageRefs);
+    printf("Page Faults = %d\n", numPageFaults);
+    printf("Page Fault Rate = %f\n", (float)numPageFaults/numPageRefs);
     return 0;
 }
